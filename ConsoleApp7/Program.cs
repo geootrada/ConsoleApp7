7bd920d9cd5842aa16648fe1fed4e49f230134ee
geootrada <geootrada@yandex.ru>
2024-02-16 14:53:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cup1 = "Кофе";
+            string cup2 = "Чай";
+            
+            Console.WriteLine($"У нас в чашке 1 - {cup1}, в чашке 2 - {cup2}.");
+            Console.WriteLine("Поменяем содержимое чашек местами");
+
+            string tempCup;
+            tempCup = cup1;
+            cup1 = cup2;
+            cup2 = tempCup;
+
+            Console.WriteLine($"Теперь у нас в чашке 1 - {cup1}, в чашке 2 - {cup2}.");
+        }
+    }
+}
